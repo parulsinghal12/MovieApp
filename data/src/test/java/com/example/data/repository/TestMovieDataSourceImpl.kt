ee14188eab1a088e1086ac1ebe0a3831359643c6
Parul Singhal <parulsinghal12@gmail.com>
2024-03-22 17:43:41 +0530

    TestDispatcher added

--- data/src/test/java/com/example/data/repository/TestMovieDataSourceImpl.kt
+++ data/src/test/java/com/example/data/repository/TestMovieDataSourceImpl.kt
@@ -8,27 +8,40 @@ import io.mockk.coEvery
 import io.mockk.mockk
 import junit.framework.TestCase.assertEquals
 import junit.framework.TestCase.fail
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.TestDispatcher
+import kotlinx.coroutines.test.TestScope
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
 import kotlinx.serialization.json.Json
+import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import retrofit2.Response
 import java.io.File
 
+@OptIn(ExperimentalCoroutinesApi::class)
 class TestMovieDataSourceImpl {
 
     private var apiService: ApiService = mockk()
     private lateinit var movieDataSource: MovieDataSourceImpl
     private val json = Json { ignoreUnknownKeys = true } // Create a Json instance
+    private val testDispatcher = UnconfinedTestDispatcher()
+    private val testScope = TestScope(testDispatcher)
+
 
     @Before
     fun setUp() {
-        movieDataSource = MovieDataSourceImpl(apiService)
+        movieDataSource = MovieDataSourceImpl(apiService, testDispatcher)
     }
 
     @Test
-    fun `getMovies returns success`() = runTest {
+    fun `getMovies returns success`() = testScope.runTest {
         val mockMovieDataString = getJson("mocked_movies.json")
         val mockMovieData = json.decodeFromString<MovieListDto>(mockMovieDataString)
         val expectedMovieList = mockMovieData.toDomainMovieList() // Use the extension function to convert to domain model
@@ -48,6 +61,10 @@ class TestMovieDataSourceImpl {
     }
 
 
+    @After
+    fun tearDown() {
+
+    }
 
     private fun getJson(path: String): String {
         val resourceAsStream = this::class.java.classLoader?.getResourceAsStream(path)
