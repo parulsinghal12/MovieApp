b9e4e55116bd991580e13afd4f9afedbce2cf27e
Parul Singhal <parulsinghal12@gmail.com>
2024-04-02 01:03:46 +0530

    Feedback:App layer 1)We have added our activity as a part of app’s layer manifest fie. Is this possible to move this to respective layer? 2)App Layer should have only Application class so please remove the unused code like Theme, Color and Type 3)Please remove all unrelated dependencies as well

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,22 +56,6 @@ dependencies {
     implementation(project(mapOf("path" to ":data")))
     implementation(project(mapOf("path" to ":presentation")))
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
-
     //Hilt
     implementation(libs.hilt.android)
     kapt(libs.hilt.android.compiler)
