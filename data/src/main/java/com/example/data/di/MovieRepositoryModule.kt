4e9cafb8aa21522db9025f07d7d31c8ff82747d6
Parul Singhal <parulsinghal12@gmail.com>
2024-04-02 04:57:50 +0530

    unused imports

--- data/src/main/java/com/example/data/di/MovieRepositoryModule.kt
+++ data/src/main/java/com/example/data/di/MovieRepositoryModule.kt
@@ -4,10 +4,8 @@ import com.example.data.repository.MovieRepositoryImpl
 import com.example.domain.repository.MovieRepository
 import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
-import dagger.hilt.components.SingletonComponent
 
 @Module
 @InstallIn(ViewModelComponent::class)
