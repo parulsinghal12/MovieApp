7a9dc58146d33cebab2e1d2e6749fad0ec1806ab
Parul Singhal <parulsinghal12@gmail.com>
2024-04-04 17:36:57 +0530

    unused code removed

--- data/src/test/java/com/example/data/mapper/MovieDetailsDtoToMovieDetailTest.kt
+++ data/src/test/java/com/example/data/mapper/MovieDetailsDtoToMovieDetailTest.kt
@@ -17,22 +17,6 @@ class MovieDetailsDtoToMovieDetailTest {
 
           val movieDetail = movieDetailDto.toDomainMovieDetail()
 
-/*          assertEquals(85000000, movieDetail.budget)
-          assertEquals(5, movieDetail.genres.size) // Assuming there are 5 genres in the JSON
-          assertEquals("Action", movieDetail.genres.first().name)
-          assertEquals("https://www.dreamworks.com/movies/kung-fu-panda-4", movieDetail.homepage)
-          assertEquals(1011985, movieDetail.id)
-          assertEquals("en", movieDetail.originalLanguage)
-          assertEquals("https://image.tmdb.org/t/p/w500/${movieDetailDto.posterPath}", movieDetail.posterPath)
-          assertEquals(3, movieDetail.productionCompanies.size) // Assuming there are 3 production companies
-          assertEquals(176000000, movieDetail.revenue)
-          assertEquals(94, movieDetail.runtime)
-          assertEquals("Released", movieDetail.status)
-          assertEquals("Kung Fu Panda 4", movieDetail.title)
-          assertFalse(movieDetail.video)
-          assertEquals(6.9, movieDetail.rating, 0.01)
-          assertEquals(224, movieDetail.voteCount)*/
-
           // Assert that the domain model has the expected values
           assertEquals(movieDetailDto.budget, movieDetail.budget)
           assertEquals(movieDetailDto.genres.size, movieDetail.genres.size)
