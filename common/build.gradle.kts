fa8fc09e74bfa741689fe71044973b71cf7fb4ca
Parul Singhal <parulsinghal12@gmail.com>
2024-04-12 00:26:18 +0530

    Added new module as common: - to provide dispatcher module. - Any other common handling can also be added here

---
+++ common/build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
+    kotlin("kapt")
+    alias(libs.plugins.hilt)
+}
+
+android {
+    namespace = "com.example.common"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 34
+        targetSdk = 34
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    //Hilt
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.android.compiler)
+}
